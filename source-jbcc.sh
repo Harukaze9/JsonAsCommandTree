efa418aa44cc7b97a397abcce6f3ffb99d30d758
Harukaze8 <uminohiyoko@gmail.com>
2023-05-05 17:24:43 +0900

    more destructifve

--- source-jbcc.sh
+++ source-jbcc.sh
@@ -19,20 +19,19 @@ fi
 # ================================================ #
 
 count=0
+for source_json in `ls ${__jbcc_root_dir}/commands/*.json`
+do
+  local basename=$(basename "${source_json}" .json)
+  local temp_filename="${__jbcc_root_dir}/jbcc_${basename}.sh"
+  sed "s/%__jbcc_function_name%/${basename}/g" ~/code/JsonBasedCommandCompletion/jbcc.sh | sed "s#%__jbcc_source_json_path%#${source_json}#g" > ${temp_filename}
+  source ${temp_filename}
+done
+
 for source_json in `jq -r ".sources[]" ${__jbcc_root_dir}/config.json`
 do
-  echo "source ${source_json}"
-  __jbcc_source_json_path=$source_json
-  # cat ${__jbcc_root_dir}/jbcc.sh | sed 
-  # source ${__jbcc_root_dir}/jbcc.sh
-  local basename=$(basename "${__jbcc_source_json_path}" .json)
+  local basename=$(basename "${source_json}" .json)
   local temp_filename="${__jbcc_root_dir}/jbcc_${basename}.sh"
-  # sed "s/%__jbcc_function_name%/${basename}/g" ~/code/JsonBasedCommandCompletion/jbcc.sh > ${temp_filename}
-  # sed "s/%__jbcc_source_json_path%/${basename}/g" ~/code/JsonBasedCommandCompletion/jbcc.sh > ${temp_filename}
-  sed "s/%__jbcc_function_name%/${basename}/g" ~/code/JsonBasedCommandCompletion/jbcc.sh | sed "s#%__jbcc_source_json_path%#${__jbcc_source_json_path}#g" > ${temp_filename}
-  # sed "s/%__jbcc_source_json_path%/${__jbcc_source_json_path}/g"  ~/code/JsonBasedCommandCompletion/jbcc.sh > ${temp_filename}
-  # sed "s/%__jbcc_function_name%/${basename}/g" ~/code/JsonBasedCommandCompletion/jbcc.sh | sed "s/%__jbcc_source_json_path%/${__jbcc_source_json_path}/g" > ${temp_filename}
-  echo "write file to ${temp_filename}"
+  sed "s/%__jbcc_function_name%/${basename}/g" ~/code/JsonBasedCommandCompletion/jbcc.sh | sed "s#%__jbcc_source_json_path%#${source_json}#g" > ${temp_filename}
   source ${temp_filename}
 done
 
@@ -42,6 +41,6 @@ done
 source "${__jbcc_root_dir}/extension/store_extension.sh"
 
 # loads cdd: super useful short facade command of 'jj goto xxx'
-source "${__jbcc_root_dir}/extension/cdd_extension.sh"
+# source "${__jbcc_root_dir}/extension/cdd_extension.sh"
 
 # ==============================================================
