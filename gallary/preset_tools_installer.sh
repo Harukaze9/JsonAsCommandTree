14f0bea4bf0d532b044bd59cd50e406c3bd26733
Harukaze9 <uminohiyoko@gmail.com>
2023-05-08 03:39:59 +0900

    fix installer

--- gallary/preset_tools_installer.sh
+++ gallary/preset_tools_installer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-jact install jj.json
-jact install cdd.json
-jact install store_extension.sh
+scipt_dir=`dirname $(realpath ${0})`
+cd $scipt_dir/..
+source source-jact.sh
+cd gallary
+jact install jj.json >/dev/null
+jact install cdd.json >/dev/null
+jact install store_extension.sh >/dev/null
+
+echo "Installation finished! See \"jact list\""
