21ff727039fe0434dedf597da03fff7256a2a276
Harukaze9 <uminohiyoko@gmail.com>
2023-08-16 01:33:53 +0900

    refactored by chatGPT4

--- jact-helper.sh
+++ jact-helper.sh
@@ -1,49 +1,81 @@
-source_json_path=$1
-raw_static_path=$2
-shift
-shift
+#!/bin/bash
 
+source_json_path="$1"
+raw_static_path="$2"
+shift 2
 args=("$@")
-static_path="."
-new_command=""
-is_remove=0
-is_list=0
-for (( i=0; i<=$#; i++ ));
-do
-    arg=${args[$i]}
-    if [ "$arg" = "--add" ]; then
-        ((i++))
-        new_command=${args[$i]}
-        break
-    elif [ "$arg" = "--remove" ]; then
-        is_remove=1
-        break
-    elif [ "$arg" = "--list" ]; then
-        is_list=1
-        break
-    fi
-    static_path+=".\"$arg\""
-done
-static_path=`echo $static_path | sed 's/\.\./\./g'`
 
-if [[ -n ${new_command} ]]; then
-    new_command=$(printf "\"%s\"" "$(echo ${new_command} | sed 's/"/\\"/g')")
-    result=`jq "${static_path}.__exec = ${new_command}" ${source_json_path}`
-    if [[ -n $result ]]; then
-    echo -E $result > $source_json_path
-    fi
-elif [ $is_remove = 1 ]; then
-    result=`jq "del(${static_path})" ${source_json_path}`
-    static_path=`echo ${static_path} | sed 's/\.[^\.]*"$//'`
-    while [ -n "$static_path" ] && [ "`echo $result | jq ${static_path}`" = "{}" ]
-    do
-        result=`jq "del(${static_path})" ${source_json_path}`
-        static_path=`echo ${static_path} | sed 's/\.[^\.]*"$//'`
+get_static_path() {
+    local static_path="."
+    for arg in "${args[@]}"; do
+        case "$arg" in
+            "--add" | "--remove" | "--list")
+                break
+                ;;
+            *)
+                static_path+=".\"$arg\""
+                ;;
+        esac
     done
-    echo -E $result > $source_json_path
-elif [ $is_list = 1 ]; then
-    echo "all executable subcommands at [`echo $raw_static_path | sed 's/ --list//'`]"
-    jq ${static_path} ${source_json_path} | jq 'path(..) as $p | select(getpath($p)? | objects? and has("__exec")) | {($p | join(".")): (getpath($p).__exec)}' | jq -s 'add'
-else
-    echo "JACT Error: no path is defined at [$@] in $source_json_path"
-fi
+    echo "$static_path" | sed 's/\.\./\./g'
+}
+
+handle_add() {
+    local new_command_escaped=$(printf "\"%s\"" "$(echo $1 | sed 's/"/\\"/g')")
+    local result=$(jq "${2}.__exec = ${new_command_escaped}" "$source_json_path")
+    echo -E "$result" > "$source_json_path"
+}
+
+handle_remove() {
+    local path="$1"
+    local result=$(jq "del(${path})" "$source_json_path")
+    while [[ -n "$path" && "`echo $result | jq ${path}`" == "{}" ]]; do
+        result=$(jq "del(${path})" "$source_json_path")
+        path=$(echo "$path" | sed 's/\.[^\.]*"$//')
+    done
+    echo -E "$result" > "$source_json_path"
+}
+
+handle_list() {
+    local list_prefix=$(echo "$raw_static_path" | sed 's/ --list//')
+    echo "all executable subcommands at [$list_prefix]"
+    jq "$1" "$source_json_path" | jq 'path(..) as $p | select(getpath($p)? | objects? and has("__exec")) | {($p | join(".")): (getpath($p).__exec)}' | jq -s 'add'
+}
+
+main() {
+    local static_path=$(get_static_path)
+    local operation=""
+    local new_command_value=""
+
+    for (( i=0; i<${#args[@]}; i++ )); do
+        case "${args[$i]}" in
+            "--add")
+                operation="add"
+                new_command_value="${args[$((i+1))]}"
+                ;;
+            "--remove")
+                operation="remove"
+                ;;
+            "--list")
+                operation="list"
+                ;;
+        esac
+    done
+
+    case "$operation" in
+        "add")
+            handle_add "$new_command_value" "$static_path"
+            ;;
+        "remove")
+            handle_remove "$static_path"
+            ;;
+        "list")
+            handle_list "$static_path"
+            ;;
+        *)
+            echo "JACT Error: no path is defined at [$@] in $source_json_path"
+            ;;
+    esac
+}
+
+main
